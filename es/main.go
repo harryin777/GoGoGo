b9d00b23dc9335a41e23f99c7beb11a3198bad3f
harryin777 <>
2022-05-13 19:11:19 +0800

    2022.5.12: add test

--- es/main.go
+++ es/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/olivere/elastic/v7"
 	"github.com/olivere/elastic/v7/config"
+	"github.com/ysmood/got/lib/gop"
 	"sync"
 	"test1/common/loader"
 )
@@ -23,12 +24,17 @@ func init() {
 func main() {
 	ctx := context.Background()
 
-	client, err := elastic.NewClientFromConfig(cfg)
+	//client, err := elastic.NewClientFromConfig(cfg)
+	//sniff 集群导致请求超时
+	//client, err := elastic.NewClient(elastic.SetURL(cfg.URL), elastic.SetBasicAuth(cfg.Username, cfg.Password), elastic.SetSnifferTimeout(1*time.Nanosecond))
+	//停止连接集群时嗅探
+	client, err := elastic.NewClient(elastic.SetURL(cfg.URL), elastic.SetBasicAuth(cfg.Username, cfg.Password), elastic.SetSniff(false))
 	if err != nil {
 		// Handle error
 		panic(err)
 	}
 	fmt.Println("connected to elasticsearch")
+	//检查索引存在
 	exists, err := client.IndexExists("oppo_info").Do(ctx)
 	if err != nil {
 		// Handle error
@@ -36,4 +42,9 @@ func main() {
 	}
 	fmt.Println(exists)
 
+	result, err := client.Refresh("oppo_info").Do(ctx)
+	if err != nil {
+		panic(err)
+	}
+	gop.P(result)
 }
