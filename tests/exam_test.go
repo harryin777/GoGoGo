d83d291abd6b47c01d22faec98712bc5029d69e4
harryin777 <>
2023-02-09 18:25:39 +0800

    update

--- tests/exam_test.go
+++ tests/exam_test.go
@@ -366,3 +366,32 @@ func Test_Exam9(t *testing.T) {
 		fmt.Println("something else")
 	}
 }
+
+type query func(string) string
+
+func exec(name string, vs ...query) string {
+	ch := make(chan string)
+	fn := func(i int) {
+		fmt.Printf("here : %v \n", i)
+		ch <- vs[i](name)
+	}
+	fmt.Println(len(vs))
+	for i, _ := range vs {
+		go fn(i)
+	}
+	return <-ch
+}
+
+func Test10(t *testing.T) {
+	ret := exec("111", func(n string) string {
+		return n + "func1"
+	}, func(n string) string {
+		return n + "func2"
+	}, func(n string) string {
+		return n + "func3"
+	}, func(n string) string {
+		return n + "func4"
+	})
+
+	fmt.Println(ret)
+}
--- tests/mutex_test.go
+++ tests/mutex_test.go
@@ -31,3 +31,26 @@ func TestMutex1(t *testing.T) {
 	fmt.Println("unlocked (G0)")
 	wg.Wait()
 }
+
+const (
+	// 1 左移 0 位
+	mutexLocked = 1 << iota // mutex is locked
+	// 1 左移 1 位
+	mutexWoken
+	// 1 左移 2 位
+	mutexStarving
+	mutexWaiterShift      = iota
+	starvationThresholdNs = 1e6
+)
+
+func Test1(t *testing.T) {
+	fmt.Println(mutexLocked)
+	fmt.Println(mutexWoken)
+	fmt.Println(mutexStarving)
+	fmt.Println(mutexWaiterShift)
+	fmt.Println(starvationThresholdNs)
+	var a int
+	a = -3
+	fmt.Println(a >> 3)
+
+}
