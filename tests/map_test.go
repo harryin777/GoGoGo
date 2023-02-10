f237d76d7b9018f86753200b485e9fc9f807868f
harryin777 <>
2023-02-10 13:02:25 +0800

    update

--- tests/map_test.go
+++ tests/map_test.go
@@ -1,8 +1,10 @@
 package tests
 
 import (
+	"fmt"
 	"log"
 	"runtime"
+	"test1/Utils"
 	"testing"
 )
 
@@ -35,4 +37,74 @@ func printMemStats() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	log.Printf("Alloc = %v TotalAlloc = %v Sys = %v NumGC = %v\n", m.Alloc/1024, m.TotalAlloc/1024, m.Sys/1024, m.NumGC)
+}
+
+type student struct {
+	Name string
+	Age  int
+}
+
+func TestRange(t *testing.T) {
+	m := make(map[string]*student)
+	stus := []student{
+		{Name: "zhou", Age: 24},
+		{Name: "li", Age: 23},
+		{Name: "wang", Age: 22},
+	}
+	for _, stu := range stus {
+		// 可以看到地址一直没有变化,也就是 stu 这个变量只初始化一次,后续都是在前面的同一个地址上赋值,所以 val 不变,但是地址一直都是一样.
+		// 那么最后 map 里的值也都是一样的,因为是从同一个内存地址获取的.
+		//fmt.Printf("%p \n", &stu)
+		// 为什么这种操作可以,这是新建了一个临时局部变量,会给这个临时变量分配新的地址,值是 stu 的值,那么 map 最后指向的是临时变量的地址对应的值
+		// tmp := stu
+		m[stu.Name] = &stu
+	}
+	//map 中存放的是地址
+	//for _, val := range m {
+	//	val.Age = 999
+	//}
+	Utils.ReceiveStruct(m)
+
+	m2 := make(map[string]student)
+	for _, stu := range stus {
+		m2[stu.Name] = stu
+	}
+	//map 中存放的是值
+	for _, val := range m2 {
+		val.Age = 999
+	}
+	fmt.Printf("map2 : %v \n", m2)
+
+}
+
+func TestMapAndSlice(t *testing.T) {
+	// 测试下 slice, 初始化容量,防止扩容
+	arr1 := []int{1, 2, 3, 4, 5}
+	slice1 := make([]int, 0, 0)
+	map1 := make(map[int]int)
+
+	for _, val := range arr1 {
+		slice1 = append(slice1, val)
+		map1[val] = val
+	}
+
+	fmt.Printf("slice2 : %v \n", slice1)
+	fmt.Printf("map2 : %v \n", map1)
+
+	stus := []student{
+		{Name: "zhou", Age: 24},
+		{Name: "li", Age: 23},
+		{Name: "wang", Age: 22},
+	}
+
+	slice2 := make([]student, 0, 0)
+	map2 := make(map[string]student)
+
+	for _, val := range stus {
+		slice2 = append(slice2, val)
+		map2[val.Name] = val
+	}
+
+	fmt.Printf("slice2 : %v \n", slice2)
+	fmt.Printf("map2 : %v \n", map2)
 }
---
+++ tests/slice_test.go
@@ -0,0 +1,52 @@
+package tests
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPointer(t *testing.T) {
+	arrayA := [2]int{100, 200}
+	testArrayPoint1(&arrayA) // 1.传数组指针
+	// 注意这里, arrayB 本身是有一个新的地址没问题,但是 slice 底层是数组,slice 有三个属性,一个指针,一个长度,一个容量
+	// 指针指向数组开头,也就是说这里,arrayB 指向的地址和 arrayA 指向的地址是同一个数组
+	// 那么什么时候 arrayB 指向一个全新的数组, 在 arrayB 追加一个元素, 无论是在数组开始还是末尾, 但是对数组内元素的更改是不会导致指向一个新的数组,也就是会对原先的 slice 产生影响
+	// 如果原先的数组还够用,那么array 追加新的元素是不会指向新的数组,只是覆盖原来数组对应的索引
+	arrayB := arrayA[:]
+	// 扩容, 那么最后的结果 arrayA 的第一个元素就变成了 300
+	//arrayB = append(arrayB, 1)
+	// 所以为什么这里虽然传递的是 arrayB 的地址,但是依然会改变 arrayA 的值,因为指向的是同一个地址
+	testArrayPoint2(&arrayB) // 2.传切片
+	fmt.Printf("arrayA : %p , %v\n", &arrayA, arrayA)
+}
+
+func testArrayPoint1(x *[2]int) {
+	fmt.Printf("func Array : %p , %v\n", x, *x)
+	(*x)[1] += 100
+}
+
+func testArrayPoint2(x *[]int) {
+	fmt.Printf("func Array : %p , %v\n", x, *x)
+	(*x)[1] += 100
+}
+
+func TestInitParameter(t *testing.T) {
+	slice1 := []int{1, 2, 3, 4, 5}
+	//slice2 := slice1[0:2:2]
+	//fmt.Println(slice1)
+	//slice2[0] = 0
+	// 这时候追加会指向一个新的数组.
+	//slice2 = append(slice2, 6)
+	//slice2[1] = 9
+	//fmt.Println(slice1)
+
+	slice3 := slice1[0:2:3]
+	fmt.Println(slice1)
+	slice3[0] = 0
+	// 这时候追加会不会指向一个新的数组,因为 slice3创建的时候, max 和 cap 不一致, max为切片保留的原切片的最大下标.
+	slice3 = append(slice3, 6)
+	// 改变了初始 slice 的值.
+	slice3[1] = 9
+	fmt.Println(slice1)
+
+}
