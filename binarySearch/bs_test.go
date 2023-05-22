be09e76cf5f12c8e76cd662cb090ab00684c5619
harryyin <harryyyin@tencent.com>
2023-05-22 15:45:01 +0800

    5.22

--- binarySearch/bs_test.go
+++ binarySearch/bs_test.go
@@ -9,3 +9,12 @@ func TestLeftBound(t *testing.T) {
 	fmt.Println(LeftBound([]int{1, 2, 3, 3, 5, 6, 7}, 3))
 	fmt.Println(RightBound([]int{1, 2, 3, 3, 5, 6, 7}, 3))
 }
+
+func TestTripleOne(t *testing.T) {
+	TripleOne(2)
+}
+
+func TestNormalBSVSBS(t *testing.T) {
+	fmt.Println(NormalBS([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}, 3))
+	fmt.Println(BS([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}, 3))
+}
