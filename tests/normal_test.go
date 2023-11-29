022c5054192adf2551733d7476edf0b1444e18c4
harryyyin <harryyyin@tencent.com>
2023-11-29 11:08:50 +0800

    append 的操作

--- tests/normal_test.go
+++ tests/normal_test.go
@@ -2,7 +2,6 @@ package tests
 
 //标准输出包
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	jsoniter "github.com/json-iterator/go"
@@ -50,258 +49,6 @@ func Test_TraverseStr(t *testing.T) {
 }
 
 /**
- * @Description 测试数组
- * @Param
- * @return
- **/
-func Test_InitArray(t *testing.T) {
-	//接受参数，注意如果是字符串数组，循环出来直接就是字符串，而不是数字
-	arrP := [4]int{
-		1,
-		5,
-	}
-	for i := 0; i < len(arrP); i++ {
-		//fmt.Printf(string(arrP[i]))
-	}
-	fmt.Println()
-
-	//指定下标元素值，初始化并新建数组
-	arr1 := [...]int{1: 5, 6: 1}
-	arr2 := arr1
-	for i := 0; i < len(arr1); i++ {
-		fmt.Print(arr1[i])
-	}
-	fmt.Println()
-	arr1[1] = 999
-	for i := 0; i < len(arr2); i++ {
-		fmt.Print(arr2[i])
-	}
-	//说明新建一个数组以后内存地址是不同的
-	fmt.Println(&arr1 == &arr2)
-
-	//初始化结构体数组
-	target := [...]struct {
-		name string
-		age  uint8
-	}{
-		{"user1", 1},
-		{"user2", 10},
-	}
-	//普通循环结构体数组
-	for i := 0; i < len(target); i++ {
-		fmt.Print(target[i])
-	}
-	for key, val := range target {
-		fmt.Printf("key:%v, val: %v\n", key, val)
-	}
-
-}
-
-/**
- * @Description 测试slice
- * @Param
- * @return
- **/
-func Test_SlicePractice(t *testing.T) {
-	array1 := [...]int{1, 2, 3, 4}
-
-	slice1 := array1[:]
-	for i := 0; i < len(slice1); i++ {
-		fmt.Print(slice1[i])
-	}
-	fmt.Println()
-
-	slice2 := array1[:]
-	slice1[1] = 1000
-	for i := 0; i < len(slice1); i++ {
-		fmt.Print(slice1[i])
-	}
-	//内存地址不同，但是值却是相同的
-	fmt.Println(&slice1 == &slice2)
-	for i := 0; i < len(slice2); i++ {
-		fmt.Print(slice2[i])
-	}
-
-	//元素为map类型的切片，注意一定在最前面有个[]
-	slice3 := make([]map[string]string, 8)
-	slice3[0] = make(map[string]string, 2)
-	slice3[0]["key1"] = "val1"
-
-	//测试只声明key
-	for k := range slice3 {
-		fmt.Println(k)
-	}
-
-	//测试append一个slice给另一个slice
-	slice4 := []int{7, 77, 777}
-	slice4 = append(slice4, slice1...)
-
-}
-
-/**
- * @Description make(map[KeyType]ValueType, [cap])
- * @Param
- * @return
- **/
-func Test_Map(t *testing.T) {
-
-	userInfo := map[string]string{
-		"username": "aaa",
-		"password": "123",
-	}
-	fmt.Println(userInfo)
-	//判断键值是否存在，ok存在为true，不存在为false，v为值类型的零值
-	v, ok := userInfo["username1"]
-	if ok {
-		fmt.Println(v)
-	} else {
-		fmt.Println("nothing at all")
-	}
-	//map的遍历
-	for k, v := range userInfo {
-		fmt.Printf("%v\n %v\n", k, v)
-	}
-
-	//value为slice的map
-	mapContainSlice := make(map[string][]string, 3)
-	//初始化aaa对应的value值，长度为8
-	mapContainSlice["aaa"] = make([]string, 8)
-	fmt.Println(len(mapContainSlice["aaa"]))
-
-	userInfo2 := userInfo
-	fmt.Println(userInfo2)
-	userInfo["111"] = "111"
-	fmt.Println(userInfo)
-}
-
-type tem struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
-func Test_Struct(t *testing.T) {
-	name := "aaa"
-	age := 18
-	//新建结构体的语法
-	person := tem{Name: name, Age: age}
-	fmt.Println(person)
-
-	//匿名结构体
-	var temp struct {
-		Name string
-		Age  int
-	}
-	temp.Name = "bbb"
-	temp.Age = 18
-	fmt.Println(temp)
-
-	//这种初始化方式必须初始化所有的字段
-	p1 := tem{
-		"p1",
-		12,
-	}
-	fmt.Printf("p7=%#v\n", p1)
-
-	//使用键值对初始化可以初始化部分字段，其他字段保留零值
-	p2 := tem{
-		Name: "age",
-	}
-	fmt.Printf("p7=%#v\n", p2)
-
-	//结构体数组
-	tmpArray := []tem{
-		{
-			Name: "1",
-			Age:  1,
-		},
-		{
-			Name: "2",
-			Age:  2,
-		},
-	}
-	_ = tmpArray
-}
-
-/*
-*
-这是一个方法
-函数和方法的区别在于 函数是没有接受者，也就是名称前面那个参数
-方法的调用和函数的不同在于
-函数可以直接调用，方法必须要实例化接受者以后才可以调用
-*/
-func (t *tem) adj1(newAge int) {
-	t.Age = newAge
-}
-
-func (t tem) adj2(newAge int) {
-	t.Age = newAge
-}
-
-/**
- * @Description 测试defer
- * @Param
- * @return
- **/
-func Test_Defer(t *testing.T) {
-
-	defer func() {
-		fmt.Println("test1")
-	}()
-
-	fmt.Println("gap111")
-	defer func() {
-		fmt.Println("test2")
-	}()
-	fmt.Println("gap222")
-	defer func() {
-		fmt.Println("test3")
-	}()
-
-}
-
-/*
-*
-  - @Description 序列化测试，唯一的就是golang可以把字节数组转换成json格式的字符串，java并不行
-    因为golang的字符串是有字节组成的，并不像传统的字符串是由字符组成的
-  - @Param
-  - @return
-    *
-*/
-func Test_Str2JSON(t *testing.T) {
-	str := "[\n\t2,\n\t3\n]"
-	_ = str
-	data := []int{2, 3}
-	var result []int
-	//strBytes 是一个字节数组
-	strBytes, _ := json.Marshal(data)
-	println(strBytes)
-	//这里输出的把字节数组转换成了一个str
-	println(string(strBytes))
-	json.Unmarshal([]byte(strBytes), &result)
-	for _, v := range result {
-		println(v)
-	}
-
-	//strBytes2, _ := json.Marshal(str)
-	//println(string(strBytes2))
-}
-
-/**
- * @Description  测试高效字符串拼接
- * @Param
- * @return
- **/
-func Test_BiteBuffer(t *testing.T) {
-	var b bytes.Buffer
-
-	for i := 0; i < 1000; i++ {
-		b.WriteString("123")
-	}
-
-	fmt.Println(b.String())
-}
-
-/**
 * @Description: 测试int类型
 * @Param:
 * @return:
@@ -312,18 +59,6 @@ func Test_Int(t *testing.T) {
 	fmt.Println(fmt.Sprintf("原字符：%c, 对应码值：%d", a, a))
 	fmt.Println("a 占用的空间 = ", unsafe.Sizeof(a), "字节")
 
-}
-
-/**
- * @Description 判断结构体是否为空
- * @Param
- * @return
- **/
-func Test_StructIsEmptyOrNot(t *testing.T) {
-	var temp tem
-	if (temp == tem{}) {
-		fmt.Println("为空")
-	}
 }
 
 /**
@@ -546,4 +281,13 @@ func Test_Marshal(t *testing.T) {
 	person := Person2{name: "polarisxu", hobby: "Golang"}
 	m, _ := json.Marshal(person)
 	fmt.Printf("%s", m)
+}
+
+func getPersonName(level int, p []Person) (res []string) {
+	res = make([]string, 0, 10)
+	name := p[level].Name
+	res = append(res, name)
+	res2 := getPersonName(level+1, p)
+	res = append(res, res2...)
+	return
 }
--- tests/slice_test.go
+++ tests/slice_test.go
@@ -2,12 +2,12 @@ package tests
 
 import (
 	"fmt"
-	"sort"
 	"test1/Utils"
 	"testing"
 )
 
 func TestPointer(t *testing.T) {
+	// 这是个数组
 	arrayA := [3]int{100, 200, 300}
 	testArrayPoint1(&arrayA) // 1.传数组指针
 	// 注意这里, arrayB 本身是有一个新的地址没问题,但是 slice 底层是数组,slice 有三个属性,一个指针,一个长度,一个容量
@@ -15,10 +15,14 @@ func TestPointer(t *testing.T) {
 	// 那么什么时候 arrayB 指向一个全新的数组, 在 arrayB 追加一个元素, 无论是在数组开始还是末尾, 但是对数组内元素的更改是不会导致指向一个新的数组,也就是会对原先的 slice 产生影响
 	// 如果原先的数组还够用,那么array 追加新的元素是不会指向新的数组,只是覆盖原来数组对应的索引
 	arrayB := arrayA[:1:2]
+	fmt.Printf("截取之后 : %v \n", arrayB)
 	// 扩容,而且扩容必须要超过B的capacity，B指向了新的地址，所以最后A的0位置元素不是300，因为B已经和A指向的不是同一个地址了，如果注释掉这一段，还是300
 	arrayB = append(arrayB, 1, 1)
+	// 不会扩容
+	//arrayB = append(arrayB, 1)
 	// 所以为什么这里虽然传递的是 arrayB 的地址,但是依然会改变 arrayA 的值,因为指向的是同一个地址
 	testArrayPoint2(&arrayB) // 2.传切片
+	testArrayPoint3(arrayB)  // 2.传切片
 	fmt.Printf("arrayA : %p , %v\n", &arrayA, arrayA)
 	fmt.Printf("arrayB : %p , %v\n", &arrayB, arrayB)
 }
@@ -33,9 +37,40 @@ func testArrayPoint2(x *[]int) {
 	(*x)[0] += 100
 }
 
+func testArrayPoint3(x []int) {
+	fmt.Printf("func test3 Array : %p , %v\n", x, x)
+	x[0] += 100
+}
+
+/*
+change 函数的参数s是原始切片slice的一个副本，它们共享同一个底层数组，但是切片的长度和容量信息是各自独立的
+第一次进入 change, 参数 s 的长度和容量分别是 3,5, append 操作不会引发扩容, 所以不会指向新的数组, 底层数组从原来的 1,2,0,0,0 变成了 1,2,0,3,4
+第二次进入 change, 参数 s 的长度和容量分别是 2,2, append 操作会引发扩容, 所以会指向新的数组, 底层数组从原来的 1,2,0,3,4 变成了 1,2,3,4,0
+外层的 slice 指向新的数组, 但是长度和容量和change 内的 s 是独立的
+*/
+func TestAppend(t *testing.T) {
+	slice := make([]int, 3, 5)
+	slice[0] = 1
+	slice[1] = 2
+	fmt.Printf("slice 1: %p , %v\n", slice, slice)
+	//slice = append(slice, 3)
+	change(slice)
+	fmt.Printf("slice 2: %p , %v\n", slice, slice)
+	change(slice[0:2])
+	fmt.Printf("slice 3: %p , %v\n", slice, slice)
+
+}
+
+func change(s []int) {
+	fmt.Printf("change before: %p , %v\n", s, s)
+	s = append(s, 3)
+	s = append(s, 4)
+	fmt.Printf("change after: %p , %v\n", s, s)
+}
+
 func TestInitParameter(t *testing.T) {
 	slice1 := []int{1, 2, 3, 4, 5}
-	// 注意这里已经指定了容量为 2,那么 cap = 2 - 0 = 2,如果不指定,那么 cap = 原 slice 的长度 - 0 = 5
+	// 注意这里已经指定了容量为 2
 	slice2 := slice1[0:2:2]
 	fmt.Printf("1 slice1:%v \n", slice1)
 	fmt.Printf("2 slice2:%v \n", slice2)
@@ -60,25 +95,6 @@ func TestInitParameter(t *testing.T) {
 	fmt.Printf("9 slice1:%v \n", slice1)
 	fmt.Printf("10 slice3:%v \n", slice3)
 
-}
-
-func TestQuotation(t *testing.T) {
-	slice := make([]int, 0, 10)
-	updateSlice(slice)
-	fmt.Println(slice)
-}
-
-func updateSlice(s []int) {
-	s = append(s, 1)
-}
-
-func TestSortSlice(t *testing.T) {
-	slice1 := []int{1, 5, 6, 20, 4, 2}
-	sort.Slice(slice1, func(i, j int) bool {
-		return slice1[i] < slice1[j]
-	})
-
-	fmt.Println(slice1)
 }
 
 func TestShallowCopy(t *testing.T) {
@@ -127,8 +143,12 @@ func TestAssignIndex(t *testing.T) {
 func TestInitArray(t *testing.T) {
 	a := [...]int{1, 2}
 	b := [2]int{1, 2}
+	c := []int{1, 2}
+	_ = c
 	fmt.Println(len(a))
 	fmt.Println(a == b)
+	// c 是一个 slice a,b 都是数组
+	//fmt.Println(a == c)
 }
 
 /*
@@ -169,11 +189,12 @@ func Test_addressSlice(t *testing.T) {
 		if info, ok := AMapInfo[val.Id]; ok {
 			info.B = append(info.B, val.B...)
 		} else {
-			s1 = append(s1, &A{
+			tmp := &A{
 				Id: val.Id,
 				B:  val.B,
-			})
-			AMapInfo[val.Id] = s1[len(s1)-1]
+			}
+			s1 = append(s1, tmp)
+			AMapInfo[val.Id] = tmp
 		}
 	}
 
