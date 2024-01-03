52055863ee04471d504b5ac5fdefa33790c8eb25
harryyyin <harryyyin@tencent.com>
2024-01-03 17:40:38 +0800

    1.3

--- tests/map_test.go
+++ tests/map_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"fmt"
+	jsoniter "github.com/json-iterator/go"
 	"log"
 	"runtime"
 	"test1/Utils"
@@ -130,4 +131,109 @@ func TestMapAppend(t *testing.T) {
 		m1["1"] = append(m1["1"], s)
 	}
 	fmt.Println(m1)
+}
+
+type Group struct {
+	ParamID    int         `json:"paramId"`
+	Operator   int         `json:"operator"`
+	ParamValue interface{} `json:"paramValue"`
+	Groups     []Group     `json:"groups"`
+	Relation   string      `json:"relation"`
+}
+
+type RuleObjectDetailRsp struct {
+	RuleObjectKey     string `json:"ruleObjectKey"`
+	BusinessKey       string `json:"businessKey"`
+	RuleObjectName    string `json:"ruleObjectName"`
+	LastUseTime       string `json:"lastUseTime"`
+	Creator           string `json:"creator"`
+	Updater           string `json:"updater"`
+	CreateTime        string `json:"createTime"`
+	UpdateTime        string `json:"updateTime"`
+	IsHaveDraftData   bool   `json:"isHaveDraftData"`
+	RuleObjectContent struct {
+		Use struct {
+			Rules []struct {
+				ConditionRoot struct {
+					ParamID    int         `json:"paramId"`
+					Operator   int         `json:"operator"`
+					ParamValue interface{} `json:"paramValue"`
+					Relation   string      `json:"relation"`
+					Groups     []struct {
+						ParamID    int         `json:"paramId"`
+						Operator   int         `json:"operator"`
+						ParamValue interface{} `json:"paramValue"`
+						Groups     []struct {
+							ParamID    int         `json:"paramId"`
+							Operator   int         `json:"operator"`
+							ParamValue interface{} `json:"paramValue"`
+							Groups     []string    `json:"groups"`
+							Relation   string      `json:"relation"`
+						} `json:"groups"`
+						Relation string `json:"relation"`
+					} `json:"groups"`
+				} `json:"conditionRoot"`
+				Result []struct {
+					ParamID    int         `json:"paramId"`
+					ParamKey   string      `json:"paramKey"`
+					ParamName  string      `json:"paramName"`
+					ParamValue interface{} `json:"paramValue"`
+					DataType   string      `json:"dataType"`
+				} `json:"result"`
+				ElseResults []struct {
+					ParamID    int         `json:"paramId"`
+					ParamKey   string      `json:"paramKey"`
+					ParamName  string      `json:"paramName"`
+					ParamValue interface{} `json:"paramValue"`
+				} `json:"elseResults"`
+				Name string `json:"name"`
+			} `json:"rules"`
+		} `json:"use"`
+		Draft struct {
+			Rules []struct {
+				ConditionRoot struct {
+					ParamID    string `json:"paramId"`
+					Operator   int    `json:"operator"`
+					ParamValue string `json:"paramValue"`
+					Relation   string `json:"relation"`
+					Groups     []struct {
+						ParamID    int         `json:"paramId"`
+						Operator   int         `json:"operator"`
+						ParamValue interface{} `json:"paramValue"`
+						Groups     []struct {
+							ParamID    int         `json:"paramId"`
+							Operator   int         `json:"operator"`
+							ParamValue interface{} `json:"paramValue"`
+							Groups     []string    `json:"groups"`
+							Relation   string      `json:"relation"`
+						} `json:"groups"`
+						Relation string `json:"relation"`
+					} `json:"groups"`
+				} `json:"conditionRoot"`
+				Result []struct {
+					ParamID    int         `json:"paramId"`
+					ParamKey   string      `json:"paramKey"`
+					ParamName  string      `json:"paramName"`
+					ParamValue interface{} `json:"paramValue"`
+				} `json:"result"`
+				ElseResults []struct {
+					ParamID    int         `json:"paramId"`
+					ParamKey   string      `json:"paramKey"`
+					ParamName  string      `json:"paramName"`
+					ParamValue interface{} `json:"paramValue"`
+				} `json:"elseResults"`
+				Name string `json:"name"`
+			} `json:"rules"`
+		} `json:"draft"`
+	} `json:"ruleObjectContent"`
+}
+
+func Test12111(t *testing.T) {
+	var tmp RuleObjectDetailRsp
+	str := "{\"version\":\"1.0\",\"timestamp\":1704269762,\"eventId\":\"197013e1-fafc-4f7d-9de8-7b67c7075b5e\",\"componentName\":\"QC_MIXSERVICE\",\"returnValue\":0,\"returnCode\":0,\"returnMessage\":\"ok\",\"interface\":\"mixservice.rule.getRuleObjectDetailByTypeForApi\",\"data\":{\"ruleObjectKey\":\"bms_industry_tree20240103168175\",\"businessKey\":\"bms_industry_tree\",\"ruleObjectName\":\"医疗行业与区域销售对应财管特殊审批人\",\"creator\":\"doreenlong\",\"updater\":\"doreenlong\",\"createTime\":\"2024-01-03 16:10:24\",\"updateTime\":\"2024-01-03 16:16:00\",\"isHaveDraftData\":false,\"ruleObjectContent\":{\"draft\":{},\"use\":{\"rules\":[{\"conditionRoot\":{\"id\":19466,\"ruleId\":2152,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19467,\"ruleId\":2152,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":53582,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19467}],\"nodeId\":19466},\"result\":[{\"paramId\":517,\"paramValue\":[\"sherrihuang\",\"fredxzhang\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19468,\"ruleId\":2153,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19469,\"ruleId\":2153,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":62586,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19469}],\"nodeId\":19468},\"result\":[{\"paramId\":517,\"paramValue\":[\"sherrihuang\",\"fredxzhang\",\"victormeng\",\"amayayang\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19470,\"ruleId\":2154,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19471,\"ruleId\":2154,\"isRoot\":2,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19472,\"ruleId\":2154,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":62587,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19472},{\"id\":19473,\"ruleId\":2154,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":48316,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19473},{\"id\":19474,\"ruleId\":2154,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":60630,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19474}],\"nodeId\":19471}],\"nodeId\":19470},\"result\":[{\"paramId\":517,\"paramValue\":[\"vincentngwu\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19475,\"ruleId\":2155,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19476,\"ruleId\":2155,\"isRoot\":2,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19477,\"ruleId\":2155,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":37319,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19477},{\"id\":19478,\"ruleId\":2155,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":60628,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19478},{\"id\":19479,\"ruleId\":2155,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":62584,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19479},{\"id\":19480,\"ruleId\":2155,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":29682,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19480}],\"nodeId\":19476}],\"nodeId\":19475},\"result\":[{\"paramId\":517,\"paramValue\":[\"victormeng\",\"amayayang\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19481,\"ruleId\":2156,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19482,\"ruleId\":2156,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":1,\"paramValue\":62588,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19482}],\"nodeId\":19481},\"result\":[{\"paramId\":517,\"paramValue\":[\"vincentngwu\",\"victormeng\",\"amayayang\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19483,\"ruleId\":2157,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19484,\"ruleId\":2157,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":13,\"paramValue\":28290,\"createTime\":\"2024-01-03 16:16:00\",\"updateTime\":\"2024-01-03 16:16:00\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19484}],\"nodeId\":19483},\"result\":[{\"paramId\":517,\"paramValue\":[\"olleykong\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19485,\"ruleId\":2158,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:01\",\"updateTime\":\"2024-01-03 16:16:01\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19486,\"ruleId\":2158,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":13,\"paramValue\":28291,\"createTime\":\"2024-01-03 16:16:01\",\"updateTime\":\"2024-01-03 16:16:01\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19486}],\"nodeId\":19485},\"result\":[{\"paramId\":517,\"paramValue\":[\"eevayuan\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19487,\"ruleId\":2159,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:01\",\"updateTime\":\"2024-01-03 16:16:01\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19488,\"ruleId\":2159,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":13,\"paramValue\":45160,\"createTime\":\"2024-01-03 16:16:01\",\"updateTime\":\"2024-01-03 16:16:01\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19488}],\"nodeId\":19487},\"result\":[{\"paramId\":517,\"paramValue\":[\"sherrihuang\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19489,\"ruleId\":2160,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:01\",\"updateTime\":\"2024-01-03 16:16:01\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19490,\"ruleId\":2160,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":13,\"paramValue\":25667,\"createTime\":\"2024-01-03 16:16:01\",\"updateTime\":\"2024-01-03 16:16:01\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19490}],\"nodeId\":19489},\"result\":[{\"paramId\":517,\"paramValue\":[\"jackyzhuang\",\"shirleyxliu\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"},{\"conditionRoot\":{\"id\":19491,\"ruleId\":2161,\"isRoot\":1,\"relation\":\"or\",\"paramId\":0,\"operator\":0,\"paramValue\":\"\",\"createTime\":\"2024-01-03 16:16:01\",\"updateTime\":\"2024-01-03 16:16:01\",\"paramKey\":null,\"paramName\":null,\"useType\":null,\"dataType\":null,\"source\":null,\"inputType\":null,\"isValid\":null,\"groups\":[{\"id\":19492,\"ruleId\":2161,\"isRoot\":2,\"relation\":\"\",\"paramId\":510,\"operator\":13,\"paramValue\":28481,\"createTime\":\"2024-01-03 16:16:01\",\"updateTime\":\"2024-01-03 16:16:01\",\"paramKey\":\"unitId\",\"paramName\":\"组织架构\",\"useType\":\"rule\",\"dataType\":\"int\",\"source\":\"common\",\"inputType\":\"unit\",\"isValid\":1,\"groups\":[],\"nodeId\":19492}],\"nodeId\":19491},\"result\":[{\"paramId\":517,\"paramValue\":[\"jackyzhuang\",\"shirleyxliu\",\"daisytu\"],\"paramKey\":\"rtxs\",\"paramName\":\"个人\",\"dataType\":\"string_array\"}],\"name\":\"\"}],\"labels\":[],\"elseResults\":[]}}}}"
+	err := jsoniter.UnmarshalFromString(str, &tmp)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("%+v", tmp)
 }
